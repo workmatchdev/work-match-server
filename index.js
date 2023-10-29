ba3a57e61aef3c055163684858f9b0f356f402d7
josealmz <joseharry1921@gmail.com>
2023-10-29 15:20:52 -0600

    update files

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ app.use(cors());
 // puerto de la app
 const PORT = process.env.PORT || 4000;
 
-app.use('/api/activeMemberships',require('./routes/activeMemberships'));
-app.use('/api/memberships',require('./routes/memberships'));
-app.use('/api/applicants',require('./routes/applicants'));
-app.use('/api/companies',require('./routes/companies'));
-app.use('/api/admins',require('./routes/admins'));
-app.use('/api/roles',require('./routes/roles'));
-app.use('/api/auth',require('./routes/auth'));
-// app.use('/api/jobs',require('./routes/jobs'));
+// app.use('/api/activeMemberships',require('./routes/activeMemberships'));
+// app.use('/api/memberships',require('./routes/memberships'));
+// app.use('/api/applicants',require('./routes/applicants'));
+// app.use('/api/companies',require('./routes/companies'));
+// app.use('/api/admins',require('./routes/admins'));
+// app.use('/api/roles',require('./routes/roles'));
+// app.use('/api/auth',require('./routes/auth'));
+// // app.use('/api/jobs',require('./routes/jobs'));
 
 app.listen(PORT, () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
