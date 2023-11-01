3acc87e21a1d55ff1204d50b1ec2f77eb87680b1
josealmz <joseharry1921@gmail.com>
2023-11-01 16:23:27 -0600

    test

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ const cors = require('cors');
 const app = express();
 
 // Conectar a la base de datos
-// conectarDB();
+conectarDB();
 
-// // Hablitar express.json
-// app.use(express.json({extend:true,limit: '50mb'}));
+// Hablitar express.json
+app.use(express.json({extend:true,limit: '50mb'}));
  
-// // Habilitar cors
+// Habilitar cors
 
-// app.use(cors());
+app.use(cors());
 
 // puerto de la app
 const PORT = process.env.PORT || 4000;
 
-// app.use('/api/activeMemberships',require('./routes/activeMemberships'));
-// app.use('/api/memberships',require('./routes/memberships'));
-// app.use('/api/applicants',require('./routes/applicants'));
-// app.use('/api/companies',require('./routes/companies'));
-// app.use('/api/admins',require('./routes/admins'));
-// app.use('/api/roles',require('./routes/roles'));
-// app.use('/api/auth',require('./routes/auth'));
-// app.use('/api/jobs',require('./routes/jobs'));
+app.use('/api/activeMemberships',require('./routes/activeMemberships'));
+app.use('/api/memberships',require('./routes/memberships'));
+app.use('/api/applicants',require('./routes/applicants'));
+app.use('/api/companies',require('./routes/companies'));
+app.use('/api/admins',require('./routes/admins'));
+app.use('/api/roles',require('./routes/roles'));
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/jobs',require('./routes/jobs'));
 
 app.listen(PORT, () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
