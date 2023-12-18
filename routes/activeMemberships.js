bd02be84755bc6082fe84777c40e8bca354e7975
josealmz <joseharry1921@gmail.com>
2023-12-18 03:09:20 -0600

    update files

--- routes/activeMemberships.js
+++ routes/activeMemberships.js
@@ -3,11 +3,8 @@ const router = express.Router();
 // const auth = require('../middleware/auth');
 const activeMembershipsController = require('../controllers/activeMembershipsController');
 
-router.post('/paymentTest',activeMembershipsController.paymentTest);
+// router.post('/paymentTest',activeMembershipsController.paymentTest);
 router.post('/payment-intent-stripe',activeMembershipsController.stripePaymentInten)
 router.post('/activate',activeMembershipsController.activateMembership);
-// router.post('/create',activeMembershipsController.createMatch);
-// router.put('/update/:id',activeMembershipsController.updateMatchById);
-// router.delete('/delete/:id',activeMembershipsController.deleteMatchById);
 
 module.exports = router;
