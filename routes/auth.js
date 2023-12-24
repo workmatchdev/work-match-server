a1f20e8e547d1ea6e9ea159fffdec8e9d11a17ff
josealmz <joseharry1921@gmail.com>
2023-12-23 23:04:33 -0600

    change name

--- routes/auth.js
+++ routes/auth.js
@@ -4,24 +4,18 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const auth = require('../middleware/auth');
 
-
 //Iniciar sesions
 // api/auth
 router.post('/',
     // Validacion de API
     authController.authenticateUser);
-
 router.get('/',
     auth,
     authController.authenticatedUser
 );
-
 router.post('/user/auth/app',
     authController.authenticateUserApp
 )
-
-// router.put('/usuario',authController.cambiarUsuario);
-
 router.put('/password', authController.changePassword);
 
 module.exports = router;  
